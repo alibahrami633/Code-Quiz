7bada00f7e2cdd6c798b9158eaca93fd6ad494c7
Aliagho <ali.bahrami633@yahoo.com>
2020-03-02 15:35:06 +1100

    improving readability of the code stage 1

--- assets/bll.js
+++ assets/bll.js
@@ -4,10 +4,10 @@ $("#start-btn").on("click", function() {
     $("#start-window").css("display", "none");
     $("#questions-window").css("display", "block"); 
     
-    createQuestions();
+    createQuestionCards();
 });
 
-function createQuestions() {
+function createQuestionCards() {
     // quiz content data initiation
     var quiz = [
         {question: "What is HTML?", answers: ["Hyper Text Multiple Language", "Hyper Text Makeup Language", "Hyper Text Markup Language", "Hyper Text Madeup Language"], correct: 2},
@@ -15,12 +15,18 @@ function createQuestions() {
         {question: "Question 3 text", answers: ["answer21", "answer22", "answer23", "answer24"], correct: 1}
     ];
 
-    // question window content initaition
+    addQuestions(quiz);   
+}
+
+// question window content initaition
+function addQuestions(quiz) {
     for(var i = 0; i < quiz.length; i++) {
         var currentQuestionNumber = i + 1;
+        
         $("#questions-window").append('<section class="col-sm"><article class="row card"><div class="card-header">Question' + currentQuestionNumber +'</div><div class="card-body question-text"></div><div class="answer-text text-left"><div class="card"><ul id="answers-list'+ currentQuestionNumber +'" class="list-group list-group-flush"></ul></div></div><div class="card-footer"><button id="submit" type="button" class="btn btn-primary" value="submit">Submit the Answer</button></div></article></section>');
+
         addAnswers(quiz[i], currentQuestionNumber);      
-    }    
+    } 
 }
 
 // adding answers
