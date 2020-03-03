154a591599c4b9030b0ec99247ecf17324dbe4be
Aliagho <ali.bahrami633@yahoo.com>
2020-03-03 16:59:23 +1100

    checking the correct chosen answer - solved

--- assets/bll.js
+++ assets/bll.js
@@ -5,7 +5,36 @@ $("#start-btn").on("click", function() {
     $("#questions-window").css("display", "block"); 
     
     createQuestionCards();
+
+    // adding questions and answers with no display
+    var quiz = createQuestionCards();
+    addQuestions(quiz);
+    
+
+    // first question initiation
+    if( $("#card1") ) {
+        $("#card1").css("display", "block");
+        
+        var correctAnswer = quiz[0].correct;
+        $(".submit").on("click", function(){
+            var chosen;
+            $("input[name='answers0']").each(function() {
+                if(this.checked) chosen = this.value;                    
+            });
+            if(chosen == correctAnswer) 
+                // alert("Correct");
+                nextCard();
+            else 
+                alert("incorrect");
+        }); // continue the code from here ...
+    }
+    else 
+        console.log("err: empty question bank");
 });
+
+function nextCard() {
+    // 
+}
 
 function createQuestionCards() {
     // quiz content data initiation
@@ -14,26 +43,7 @@ function createQuestionCards() {
         {question: "Question 2 text", answers: ["answer11", "answer12", "answer13", "answer14"], correct: 3},
         {question: "Question 3 text", answers: ["answer21", "answer22", "answer23", "answer24"], correct: 1}
     ];
-
-    // adding questions and answers with no display
-    addQuestions(quiz);
-
-
-    // first question initiation
-    if( $("#card1") ) {
-        $("#card1").css("display", "block");
-        
-        var correctAnswer = quiz[0].correct;
-        $("#submit1").on("click", submitAnswer(correctAnswer)); // continue the code from here ...
-    }
-    else 
-        console.log("err: empty question bank");
-}
-
-function submitAnswer(correctAnswer) {
-    if( $("#choice" + correctAnswer).is(":checked")) {
-        alert("correct");
-    } else alert("wrong");
+    return quiz;    
 }
 
 // question window content initiation
@@ -43,16 +53,16 @@ function addQuestions(quiz) {
         
         $("#questions-window").append('<section id="card' + currentQuestionNumber +'" class="col-sm top-margin hidden"><article class="row card"><div class="card-header">Question' + currentQuestionNumber +'</div><div class="card-body question-text"></div><div class="answer-text text-left"><div class="card"><ul id="answers-list'+ currentQuestionNumber +'" class="list-group list-group-flush"></ul></div></div><div class="card-footer"><button id="submit'+ currentQuestionNumber +'" type="button" class="btn btn-primary submit">Submit the Answer</button></div></article></section>');
 
-        addAnswers(quiz[i], currentQuestionNumber);        
+        addAnswers(quiz[i], i, currentQuestionNumber);        
     } 
 }
 
 // adding answers
-function addAnswers(currentQ, currentQuestionNumber) {    
+function addAnswers(currentQ, ind, currentQuestionNumber) {    
     for(var i = 0; i < currentQ.answers.length; i++) {
         var k = i + 1;
         
-        $("#answers-list" + currentQuestionNumber).append('<li class="list-group-item"><input id="choice' + k + '" type="radio" name="answers" value="' + k + '"><label for="choice' + k + '">' + currentQ.answers[i] + '</label></li>');
+        $("#answers-list" + currentQuestionNumber).append('<li class="list-group-item"><input id="choice' + k + '" type="radio" name="answers'+ ind +'" value="' + k + '"><label for="choice' + k + '">' + currentQ.answers[i] + '</label></li>');
     }
 }
 
